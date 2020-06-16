33045c0cafe7f9c934945b669d8ac9846631abc5
MyUbuntu <yz2297@cornell.edu>
2020-06-16 06:17:04 -0700

    cpu adm ctrl

--- register_actions.py
+++ register_actions.py
@@ -10,11 +10,11 @@ cmd = 'wsk action create chameleon faas_chameleon.py --docker yz2297/chameleon_o
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'float_operation'))
-cmd = 'wsk action create float_op float_operation.py -i'
+cmd = 'wsk action create float_op float_operation.py -i --memory 512'
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'image_processing'))
-cmd = 'wsk action create image_process image_process.py --docker yz2297/python3_openwhisk --web raw -i'
+cmd = 'wsk action create image_process image_process.py --docker yz2297/python3_openwhisk --web raw -i --memory 512'
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'linpack'))
@@ -26,17 +26,17 @@ cmd = 'wsk action create matmult matmult.py --docker yz2297/python3_openwhisk --
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'pyaes'))
-cmd = 'wsk action create pyaes faas_pyaes.py --docker yz2297/pyaes_openwhisk --web raw -i'
+cmd = 'wsk action create pyaes faas_pyaes.py --docker yz2297/pyaes_openwhisk --web raw -i --memory 512'
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'video_processing'))
-cmd = 'wsk action create video_process video_process.py --docker yz2297/video_process_openwhisk --web raw -i'
+cmd = 'wsk action create video_process video_process.py --docker yz2297/video_process_openwhisk --web raw -i --memory 512'
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'ml_inference' / 'logistic_regression_review'))
-cmd = 'wsk action create lr_review lr_review.py --docker yz2297/lr_review_openwhisk --web raw -i'
+cmd = 'wsk action create lr_review lr_review.py --docker yz2297/lr_review_openwhisk --web raw -i --memory 512'
 subprocess.call(cmd, shell=True)
 
 os.chdir(str(top_dir / 'ml_inference' / 'mobilenet'))
-cmd = 'wsk action create mobilenet mobilenet.py --docker yz2297/mobilenet_openwhisk --web raw -i'
+cmd = 'wsk action create mobilenet mobilenet.py --docker yz2297/mobilenet_openwhisk --web raw -i --memory 512'
 subprocess.call(cmd, shell=True)
