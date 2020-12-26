4560ca012957e855dc279a3c1c4b917c95ec0267
yanqi <yz2297@cornell.edu>
2020-12-26 15:59:17 +0800

    misnomer

--- minio/make_minio_config.py
+++ minio/make_minio_config.py
@@ -8,5 +8,5 @@ minio_config['access_key'] = '5VCTEQOQ0GR0NV1T67GN'
 minio_config['secret_key'] = '8MBK5aJTR330V1sohz4n1i7W5Wv/jzahARNHUzi3'
 minio_config['bucket'] = 'openwhisk'
 
-with open('./minion_config.json', 'w+') as f:
+with open('./minio_config.json', 'w+') as f:
     json.dump(minio_config, f, indent=4, sort_keys=True)
