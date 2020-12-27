1f096793ed0df2992efd7dac4d0b5ce23c3b2c71
yanqi <yz2297@cornell.edu>
2020-12-27 10:27:49 +0800

    change minio port

--- minio/make_minio_config.py
+++ minio/make_minio_config.py
@@ -3,7 +3,7 @@ import os
 import json
 
 minio_config = {}
-minio_config['endpoint'] = '128.253.128.68:9001' # ath-5
+minio_config['endpoint'] = '128.253.128.68:9002' # ath-5
 minio_config['access_key'] = '5VCTEQOQ0GR0NV1T67GN'
 minio_config['secret_key'] = '8MBK5aJTR330V1sohz4n1i7W5Wv/jzahARNHUzi3'
 minio_config['bucket'] = 'openwhisk'
